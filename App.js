feaab6eb2b0d0b6f588b1868a5044548d05d7168
Harshvardhan Parihar <harshvardhanparihar@gmail.com>
2018-07-15 00:27:45 +0530

    Add native base npm package

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import Expo from "expo";
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Title, Button, Left, Right, Body, Icon, Content, Footer } from 'native-base';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <Header />
+        <Content padder>
+          <Text>
+            This is Content Section
+          </Text>
+        </Content>
+        <Footer />
+      </Container>
     );
   }
 }
